4377a803ddd6ab41069d8cceb94d51b43f24349c
SarahGuarneri <sarahguarneri97@icloud.com>
2024-01-26 09:32:05 +0100

    FILE

---
+++ src/main/java/sarahguarneri/BEU2W3/Beu2W3Application.java
@@ -0,0 +1,13 @@
+package sarahguarneri.BEU2W3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu2W3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu2W3Application.class, args);
+	}
+
+}
